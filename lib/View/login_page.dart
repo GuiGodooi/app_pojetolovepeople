6066cb60f17babb812f21c6586713c319b71c1b5
GuiGodooi <gui.go.d@hotmail.com>
2022-08-21 15:44:12 -0300

    tela 1 feita

--- lib/View/login_page.dart
+++ lib/View/login_page.dart
@@ -13,11 +13,11 @@ class _LoginState extends State<Login> {
     return Scaffold(
       backgroundColor: const Color(0xFFA901F7),
       body: SafeArea(
-        child: Stack(
+        child: Column(
           children: [
             Container(
               alignment: Alignment.topCenter,
-              height: 250,
+              height: 200,
               width: MediaQuery.of(context).size.width,
               decoration: const BoxDecoration(
                 color: Colors.white,
@@ -26,13 +26,156 @@ class _LoginState extends State<Login> {
                   bottomRight: Radius.circular(200),
                 ),
               ),
-              child: const Icon(
-                Icons.people,
-                size: 200,
+              child: const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Icon(
+                  Icons.people,
+                  size: 180,
+                ),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 1),
+                      child: Text(
+                        'Que bom que voltou!',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          height: 4,
+                        ),
+                      ),
+                    ),
+                    Form(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                        ),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.all(
+                                  10,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                hintText: 'E-mail',
+                                hintStyle: TextStyle(color: Color(0xFFA901F7)),
+                                fillColor: Colors.white,
+                                filled: true,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                disabledBorder: InputBorder.none,
+                                contentPadding: const EdgeInsets.all(
+                                  10,
+                                ),
+                                border: const OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10),
+                                  ),
+                                ),
+                                hintText: 'Senha',
+                                hintStyle:
+                                    const TextStyle(color: Color(0xFFA901F7)),
+                                fillColor: Colors.white,
+                                filled: true,
+                                suffixIcon: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.remove_red_eye,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              height: 45,
+                              width: 110,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Entrar',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xff3101B9),
+                                  ),
+                                  shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      side: const BorderSide(
+                                        width: 1.5,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Divider(
+              height: 7,
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Não possui cadastro?',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                InkWell(
+                  child: Text(
+                    'Clique aqui!',
+                    style: TextStyle(color: Colors.orange),
+                  ),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
